415491378339e6cc6803943dcdf699ac9b8fc35d
Dan <thunderusnasdermn@yandex.ru>
2024-01-08 09:17:38 +0300

    fix: router basename

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/react-mesto-auth">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 reportWebVitals();
